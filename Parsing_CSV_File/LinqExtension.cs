b14cb25986fa5d8a01142a2bfd43d84267976827
AbdulJabbaar <jabbaar.15@gmail.com>
2020-09-17 17:43:43 +0500

    Add files via upload

---
+++ Parsing_CSV_File/LinqExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Parsing_CSV_File
+{
+    public static class LinqExtension
+    {
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            if (source == null)
+                throw new ArgumentNullException();
+            if (action == null)
+                throw new ArgumentNullException();
+
+            foreach (var item in source)
+            {
+                action(item);
+            }
+        }
+    }
+}
---
+++ Parsing_CSV_File/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Parsing_CSV_File
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Class { get; set; }
+        public int Marks { get; set; }
+
+        public override string ToString()
+        {
+            return $"Full Name, {FirstName + " " + LastName}, Class = {Class}, Marks={Marks}";
+        }
+
+        public static Student ParseFideCsv(string line)
+        {
+            string[] parts = line.Split(',');
+            return new Student
+            {
+                FirstName = parts[0].Trim(),
+                LastName = parts[1].Trim(),
+                Class = int.Parse(parts[2].Trim()),
+                Marks = int.Parse(parts[3].Trim())
+            };
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileLocation = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Students.csv");
+            ParseCsv(fileLocation);
+            Console.ReadLine();
+        }
+
+        private static void ParseCsv(string fileLocation)
+        {
+            File.ReadAllLines(fileLocation)
+                .Select(Student.ParseFideCsv)
+                .Where(a => a.Class > 5)
+                .OrderByDescending(o => o.Marks)
+                .Take(10).ForEach(item => Console.WriteLine(item.ToString()));
+        }
+    }
+}
